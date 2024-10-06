59526e293741df489a43f4c482268424b4145cf5
OM192004 <ommali2672@gmail.com>
2024-10-07 00:13:03 +0530

    event adding done

--- backend/utils/uploadImages.js
+++ backend/utils/uploadImages.js
@@ -1,30 +1,10 @@
-const AWS = require('aws-sdk');
+const cloudinary = require('cloudinary').v2;
 
-AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION,
+// Configure Cloudinary with your credentials
+cloudinary.config({
+  cloud_name:"dqbsjxnsi", // Set this in your .env file
+  api_key:964724497815283, // Set this in your .env file
+  api_secret: "yVylfWTfJVidr_vSp7e-Hni1hvg", // Set this in your .env file
 });
 
-const s3 = new AWS.S3();
-
-// Upload image to S3
-const uploadImageToS3 = async (buffer, originalName) => {
-    const params = {
-        Bucket: process.env.S3_BUCKET_NAME,
-        Key: `${Date.now()}_${originalName}`,  // Unique filename
-        Body: buffer,
-        ContentType: 'image/jpeg',  // You can adjust this based on the file type
-        ACL: 'public-read',         // Allow public read access
-    };
-
-    try {
-        const data = await s3.upload(params).promise();
-        return data.Location;  // Return the URL of the uploaded image
-    } catch (err) {
-        console.error('Error uploading file to S3:', err);
-        throw err;
-    }
-};
-
-module.exports=uploadImageToS3
+module.exports = cloudinary;
